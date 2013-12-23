4171763931113b393b163fadf7c037bbc5705d79
Christoph Trattner <trattner.christoph@gmail.com>
2013-12-23 01:15:19 +0100

    AGPL

--- src/test/Pipeline.java
+++ src/test/Pipeline.java
@@ -1,22 +1,22 @@
 /*
-TagRecommender:
-A framework to implement and evaluate algorithms for the recommendation
-of tags.
-Copyright (C) 2013 Dominik Kowald
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ TagRecommender:
+ A framework to implement and evaluate algorithms for the recommendation
+ of tags.
+ Copyright (C) 2013 Dominik Kowald
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+ 
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package test;
 
@@ -44,7 +44,7 @@ public class Pipeline {
 		System.out.println(	"TagRecommender framework to implement and evaluate tag-recommender algorithms.\n" +
 			   	"Copyright (C) 2013 Dominik Kowald\n\n" + 
 			   	"This program is free software: you can redistribute it and/or modify\n" + 
-				"it under the terms of the GNU General Public License as published by\n" +
+				" it under the terms of the GNU Affero General Public License as published by\n" +
 				"the Free Software Foundation, either version 3 of the License, or\n" +
 				"(at your option) any later version.\n\n" +							
 				"This program is distributed in the hope that it will be useful,\n" +
